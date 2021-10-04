9bcd31135ce9f58fdabfacc96d46869998fc8c63
474288926 <474288926@qq.com>
2021-10-04 21:45:38 +0800

    build(deps): 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
+<style lang="stylus">
+#app {
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  box-sizing border-box
+  position relative
+  width 100%
+  height 100%
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory, Router, RouteRecordRaw } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Vuex from '@/views/Vuex.vue'
+import Test from '@/views/Test.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/vuex',
+    name: 'Vuex',
+    component: Vuex
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
+  }
+]
+
+const router: Router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
