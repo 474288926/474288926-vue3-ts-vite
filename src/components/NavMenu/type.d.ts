042b4fa60da1c27400948cd2650ff10b68498d94
yby <474288926@qq.com>
2021-10-11 14:29:21 +0800

    fix: 修复husky插件代码提交规范不生效问题

---
+++ src/components/NavMenu/type.d.ts
@@ -0,0 +1,14 @@
+// export interface NavItem {
+//   path: string
+//   name: string
+//   isActive: boolean
+// }
+
+export type NavMenuEdit = {
+  BackgroundColor?: String
+  TextColor?: String
+  ActiveTextColor?: String
+  dropDownData: Object
+  isCollapse?: Boolean
+  DefaultActive: String
+}
