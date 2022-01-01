a716087b349415308570f27d6d718ec4e4da611a
474288926 <474288926@qq.com>
2022-01-01 19:51:01 +0800

    build: 设置token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,6 +2,9 @@ import Axios, { AxiosInstance } from 'axios'
 import { ElMessage } from 'element-plus'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 NProgress.configure({ showSpinner: false })
 
@@ -66,12 +69,20 @@ const allowRequest = (req: Array<string | undefined>, url: string | undefined) =
 axios.interceptors.request.use(
   (config) => {
     NProgress.start()
+
+    /*
+       抓取token，携带到响应头
+       显示loading
+    */
+    // let token = window.sessionStorage.getItem('token')
+    // token = token ? JSON.parse(token) : ''
+    // config.headers = { token } // 携带到请求头
     let cancel: Function = () => {}
     // 设置cancelToken对象
-    config.cancelToken = new Axios.CancelToken((c) => {
+    config.cancelToken = new Axios.CancelToken((c: Function) => {
       cancel = c
     })
-    // // 阻止重复请求。当山哥哥请求未完成是，相同的请求不会进行
+    // 阻止重复请求。当山哥哥请求未完成是，相同的请求不会进行
     stopRepeatRequest(reqList, config.url, cancel, `${config.url}请求被中断`)
     /**
      * 根据你的项目实际情况来对 config 做处理
@@ -88,6 +99,14 @@ axios.interceptors.request.use(
 // 后置拦截器（获取到响应时的拦截）
 axios.interceptors.response.use(
   (response) => {
+    // /*
+    //   token过期，跳转login，保留当前地址
+    //   关闭loading
+    // */
+    // // 判断如果有错误（token过期），并且当前路径不是login，则跳转到login页面，并且将当前的全路径带过去
+    // if (response.data.err == 2 && !router.currentRoute.fullPath.includes('/login')) {
+    //   router.push({ path: '/login', query: { path: router.currentRoute.fullPath } })
+    // }
     // 增加延迟，相同请求不得在短时间内重复发送
     setTimeout(() => {
       allowRequest(reqList, response.config.url)
